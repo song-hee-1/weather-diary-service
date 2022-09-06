6e118fbdd99dc6fac0e540a971b2858e167874ef
한송희 <98_han@naver.com>
2022-09-06 10:24:37 +0900

    Feat : Diary 모델링

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Diary(models.Model):
+    diary_id = models.AutoField(primary_key=True, verbose_name="일기_id")
+    title = models.CharField(max_length=20, verbose_name="제목")
+    content = models.CharField(max_length=200, verbose_name="본문")
+    password = models.CharField(max_length=120, verbose_name="비밀번호")  # 암호화를 위한 큰 길이 지정
+    nickname = models.CharField(max_length=10, verbose_name="작성자별명")
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name="생성날짜")
+    update_date = models.DateTimeField(auto_now=True, verbose_name="수정날짜")
+
+    class Meta:
+        verbose_name = "일기"
+        verbose_name_plural = "일 목록"
+        db_table = 'diary'
+
+    def __str__(self):
+        return self.title
