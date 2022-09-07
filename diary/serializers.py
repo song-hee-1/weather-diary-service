899fca66640d73b50c2fefafc3fd2e61f6af86ba
한송희 <98_han@naver.com>
2022-09-07 21:21:13 +0900

    Feat : 비밀번호 암호화 방식 argon2에서 bcrypt로 변경 - serializer update() 메소드 기능 구현의 어려움으로 argon2에서 bcrypt로 암호화 방식 변경 - bcrypt를 이용한 암호화와 복호화를 위해 serializer create(), update() 메소드 override

--- diary/serializers.py
+++ diary/serializers.py
@@ -3,7 +3,7 @@ from rest_framework.serializers import ValidationError
 
 from .models import Diary
 
-from argon2 import PasswordHasher
+import bcrypt
 
 
 class DiarySerializer(serializers.ModelSerializer):
@@ -14,9 +14,13 @@ class DiarySerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
+
         if password is not None:
-            password = PasswordHasher().hash(password)
-        return Diary.objects.create(password=password, **validated_data)
+            encode_password = password.encode('utf-8')  # 입력받은 password는 문자형(유니코드)기 때문에 hash를 위해 바이트형으로 인코딩
+            encrypt_password = bcrypt.hashpw(encode_password, bcrypt.gensalt())
+            decode_password = encrypt_password.decode('utf-8')  # DB 필드는 문자형(유니코드)이므로 다시 유니코드로 디코딩하여 저장
+
+        return Diary.objects.create(password=decode_password, **validated_data)
 
 
 class DiaryDetailSerializer(serializers.ModelSerializer):
@@ -25,17 +29,17 @@ class DiaryDetailSerializer(serializers.ModelSerializer):
         fields = '__all__'
         extra_kwargs = {'password': {'write_only': True}}
 
-    # def update(self, instance, pk, validated_data):
-    #     password = validated_data.pop('password')
-    #     pk = self.instance.pk
-    #     diary = Diary.objects.get(pk=pk)
-    #     diary_pw = diary.password
-    #
-    #     try:
-    #        PasswordHasher().verify(diary_pw, password)
-    #     except:
-    #         msg = "비밀번호가 맞지 않거나 업데이트 할 수 없습니다."
-    #         raise ValidationError(msg)
-    #
-    #     for attr, value in validated_data.items():
-    #         setattr(instance, attr, value)
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        password = password.encode('utf-8')  # checkpw 이용을 위한 인코딩
+        diary_password = instance.password.encode('utf-8')
+
+        if not bcrypt.checkpw(password, diary_password):
+            msg = "비밀번호가 맞지 않거나 업데이트 할 수 없습니다."
+            raise ValidationError(msg)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
