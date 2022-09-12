9ffc22ed1b1e041c52599546b347159a9ecc29bc
한송희 <98_han@naver.com>
2022-09-12 17:27:07 +0900

    Feat : diary 생성시 날씨 정보도 같이 저장되도록 구현
    - diary create시 날씨 정보도 같이 저장되도록 create, perform_create override

--- diary/views.py
+++ diary/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .models import Diary
 from .serializers import DiarySerializer, DiaryDetailSerializer
 from .pagination import DiaryPagination
+from .utils import get_weather_info
 
 
 class DiaryViewSet(viewsets.ModelViewSet):
@@ -15,3 +17,15 @@ class DiaryViewSet(viewsets.ModelViewSet):
         if self.action in ("retrieve", "update", "partial_update"):
             return DiaryDetailSerializer
         return DiaryDetailSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        current_weather = get_weather_info(request)
+        self.perform_create(serializer, current_weather)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer, weather=None):
+        serializer.save(weather=weather)
